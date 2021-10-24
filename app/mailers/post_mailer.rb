cf223f4e5ca26a500ee83a1c8c87dcf4ee10731e
George Garcia <jorge.90gap@gmail.com>
2021-10-24 19:38:27 +0900

    post_mailer setted up with owner email address

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,2 +1,9 @@
 class PostMailer < ApplicationMailer
+  default from: 'jinetito_sva@hotmail.com'
+  layout 'mailer'
+
+  def post_mail(post)
+    @post = post
+    mail to: "jorge.90gap@gmail.com", subject: " Your Instagram account has been created, Welcome to the community."
+  end
 end
